b11a4fcfc48f14e8da9d8270849bb6b3c6d57d5d
DELL <DELL@DESKTOP-S48QKSA>
2022-10-24 13:42:57 +0530

    This is HelloWorld Project

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Hello {
+
+	String string = "My name is Sahil"+ "\n";
+	String string1 = "Nice to meet you";
+	
+	public static void main(String[] args) {
+		
+		System.out.println(string);
+		System.out.println(string1);
+		
+	}
+
+}
---
+++ src/String.java
@@ -0,0 +1,14 @@
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		String string,mainstring;
+		String mainstring = "Hello world";
+        String string = "Hello Universe";
+        if (mainstring.includes(string)) {
+        System.out.println("Good");
+        } else
+        	System.out.println("Bad");
+	}
+
+}
